587141aa2db0f6373f04676a99204f8fce1c7b09
Yan Grunenberger <yan@grunenberger.net>
2019-06-01 21:30:33 +0200

    Use component in the index.js

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import io from 'socket.io-client';
 import Dashboard from './Dashboard';
 
-export default () => (
-  <div>
-    <Dashboard />
-  </div>
-)
 
+// export default () => (
+//   <div>
+//     <Dashboard />
+//   </div>
+// )
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount = () => {
+      const socket = io();
+  }
+  
+  render() {
+    return (
+    <div>
+     <Dashboard />
+    </div>
+    )
+  }
+}
+
+
+export default App;
